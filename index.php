502ad766a76234e0c31b085d0f14496e434aed63
Yashvi-01 <yashvi4211@gmail.com>
2024-12-03 17:33:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World! hello from git action!</p>'; ?> 
+ <?php echo '<p>Hello World! hello from git action!hello yashvi</p>'; ?> 
  </body>
 </html>
