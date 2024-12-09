9ff626c3a248bdd015b21e87e24d36fa656997b8
Yashvi-01 <yashvi4211@gmail.com>
2024-12-09 18:07:01 +0530

    code-change

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World! this is testing!!</p>'; ?> 
+ <?php echo '<p>Hello World! This is test php code!!</p>'; ?> 
  </body>
 </html>
